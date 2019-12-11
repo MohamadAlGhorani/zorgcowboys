daa2626b6dc41c1caed2cee1007cd918269a06b1
MohamadAlGhorani <moh.gh266@gmail.com>
2019-12-11 20:46:22 +0100

    setup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,17 @@
-import ".styles/styles.scss";
+import "../styles.scss";
 import "babel-polyfill";
 console.log("hello world!");
+
+(function request() {
+    fetch("zorgcowboys.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            makeVisualization(data);
+        });
+})();
+
+function makeVisualization(data) {
+    console.log(data);
+}
