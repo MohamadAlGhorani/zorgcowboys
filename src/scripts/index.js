07fc8178f6b0ca42afb6009d03b7c3a0dd2c6ec0
MohamadAlGhorani <moh.gh266@gmail.com>
2019-12-11 20:52:15 +0100

    setup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,2 @@
 import "../styles.scss";
 import "babel-polyfill";
-console.log("hello world!");
-
-(function request() {
-    fetch("zorgcowboys.json")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            makeVisualization(data);
-        });
-})();
-
-function makeVisualization(data) {
-    console.log(data);
-}
