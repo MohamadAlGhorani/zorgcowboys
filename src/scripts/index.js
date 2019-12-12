981d333b0238f2938c6d230dadae72b6c5800c49
MohamadAlGhorani <moh.gh266@gmail.com>
2019-12-12 11:33:28 +0100

    get data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,18 @@
 import "../styles.scss";
 import "babel-polyfill";
 
-console.log("Hello world")
+console.log("Hello world");
+
+var api =
+  "https://mohamadalghorani.github.io/zorgcowboys-data/zorgcowboys.json";
+
+function runQuery(url) {
+  return fetch(url)
+    .then(res => res.json())
+    .then(data => data)
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+console.log(runQuery(api));
