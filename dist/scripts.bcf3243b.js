24deb4f1d24a10962b8449a748f386f877f70d44
Wiebe Kremer <wiebe.kremer96@gmail.com>
2020-01-07 11:39:15 +0100

    Added object transformation

--- dist/scripts.bcf3243b.js
+++ dist/scripts.bcf3243b.js
@@ -8218,8 +8218,50 @@ function clean(data) {
   console.log(data);
   var newdata = d3.nest().key(function (d) {
     return d.concerncode;
-  }).entries(data);
+  }).key(function (d) {
+    return d.jaar;
+  }).entries(data).map(function (group) {
+    console.log(group);
+    return {
+      concerncode: group.key,
+      bedrijfsnaam: group.values[0].values[0].bedrijfsnaam,
+      plaats: group.values[0].values[0].plaats,
+      zorgSoort: checkZorg(group.values[0].values[0]),
+      jaren: group.values
+    };
+  });
   console.log(newdata);
+}
+
+function checkZorg(data) {
+  // console.log("infunction:", data)
+  //
+  // Alleen Geestelijk: 1
+  // Alleen Gehandicapte: 2
+  // Alleen Thuiszorg: 3
+  // Geestelijk & Gehandicapten: 4
+  // Geestelijk & Thuiszorg: 5
+  // Gehandicapten & Thuiszorg 6
+  // Alles: 7
+  if (data.geestelijkegezondheidszorg == "ja") {
+    if (data.gehandicaptenzorg == "ja" && data.thuiszorg == "ja") {
+      return 7;
+    } else if (data.gehandicaptenzorg == "ja" && data.thuiszorg == "nee") {
+      return 4;
+    } else if (data.gehandicaptenzorg == "nee" && data.thuiszorg == "ja") {
+      return 5;
+    } else if (data.gehandicaptenzorg == "nee" && data.thuiszorg == "nee") {
+      return 1;
+    }
+  } else if (data.gehandicaptenzorg == "ja") {
+    if (data.thuiszorg == "ja") {
+      return 6;
+    } else if (data.thuiszorg == "nee") {
+      return 2;
+    }
+  } else if (data.thuiszorg == "ja") {
+    return 3;
+  }
 }
 },{"./runApi.js":"scripts/runApi.js"}],"scripts/index.js":[function(require,module,exports) {
 "use strict";
@@ -8259,7 +8301,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64784" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57470" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/scripts/cleaning.js
+++ src/scripts/cleaning.js
@@ -16,6 +16,56 @@ function clean(data) {
         .key(function (d) {
             return d.concerncode;
         })
+        .key(function (d) {
+            return d.jaar;
+        })
         .entries(data)
+        .map(function(group) {
+            console.log(group)
+		    return {
+		    	concerncode: group.key,
+                bedrijfsnaam: group.values[0].values[0].bedrijfsnaam,
+                plaats: group.values[0].values[0].plaats,
+                zorgSoort: checkZorg(group.values[0].values[0]),
+		    	jaren: group.values
+		    }
+		})
+
     console.log(newdata)
+}
+
+function checkZorg(data) {
+    // console.log("infunction:", data)
+    //
+    // Alleen Geestelijk: 1
+    // Alleen Gehandicapte: 2
+    // Alleen Thuiszorg: 3
+    // Geestelijk & Gehandicapten: 4
+    // Geestelijk & Thuiszorg: 5
+    // Gehandicapten & Thuiszorg 6
+    // Alles: 7
+    if (data.geestelijkegezondheidszorg == "ja") {
+        if (data.gehandicaptenzorg == "ja" && data.thuiszorg == "ja" ) {
+            return 7
+        }
+        else if (data.gehandicaptenzorg == "ja" && data.thuiszorg == "nee" ) {
+            return 4
+        }
+        else if (data.gehandicaptenzorg == "nee" && data.thuiszorg == "ja" ) {
+            return 5
+        }
+        else if (data.gehandicaptenzorg == "nee" && data.thuiszorg == "nee" ) {
+            return 1
+        }
+    } else if (data.gehandicaptenzorg == "ja") {
+        if (data.thuiszorg == "ja" ) {
+            return 6
+        }
+        else if (data.thuiszorg == "nee" ) {
+            return 2
+        }
+    } else if (data.thuiszorg == "ja") {
+        return 3
+    }
+
 }
